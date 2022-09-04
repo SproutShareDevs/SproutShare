cf46875814db9b3996f45529aea106b744133e4a
tserrante <tserrante@gmail.com>
2022-09-03 20:47:55 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SproutShare
+
+1. Install dependencies by using "npm install" from the SproutShare directory.
+2. Run using "npm start" from the SproutShare directory.
---
+++ app.js
@@ -0,0 +1,17 @@
+
+/**
+ * Express
+ */
+const express = require('express');
+const app = express();
+
+/**
+ * Mongoose
+ */
+const mongoose = require('mongoose');
+
+app.get('/', (req,res)=>{
+   res.send('Sprout Share...' + '\nMore Coming Soon');
+});
+
+app.listen(3000, ()=>{console.log('Listening on port 3000...')});
