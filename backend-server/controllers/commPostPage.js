d67bf31958ee4c61c3300fda668fabb7e7aaf36f
tserrante <tserrante@gmail.com>
2022-09-17 21:01:16 -0400

    merge commit

--- backend-server/controllers/commPostPage.js
+++ backend-server/controllers/commPostPage.js
@@ -8,5 +8,5 @@ const CommunityPost = require('../models/CommunityPost');
 
 module.exports = async(req, res) => {
    const commPosts = await CommunityPost.find({});
-   res.render('communityPosts', {commPosts});
+   res.send({commPosts});
 }
