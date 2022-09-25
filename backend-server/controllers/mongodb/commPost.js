6ea9c0d2d91cf8dca74e6a6c7601e81bbd41256d
Tom Serrante <tserrante@gmail.com>
2022-09-24 21:01:32 -0400

    prod community posts is now compatible with postman, except query

--- backend-server/controllers/mongodb/commPost.js
+++ backend-server/controllers/mongodb/commPost.js
@@ -16,8 +16,9 @@ router.get('/', async(req, res) => {
 /** 
  * This route gets a community post ID via parameter in the get request and sends it in EJS 
  */
-router.get('/id', async(req, res) => {
-   const commPosts = [await CommunityPosts.findById(req.query.id)];
+router.get('/:id', async(req, res) => {
+   const commPosts = await CommunityPosts.findById(req.params.id);
+   
    res.send(commPosts);
 }) 
 
@@ -31,29 +32,24 @@ router.get('/search', async(req, res) =>{
     const commPosts = await CommunityPosts.find()
     .or(
           [
-             {comm_post_title: {$regex:req.query.string}}, 
-             {comm_post_body: {$regex:req.query.string}}
+            {comm_post_title: {$regex:req.query.string}}, 
+            {comm_post_body: {$regex:req.query.string}}
           ]
     );
-    res.send('communityPosts', {commPosts});
+    res.send(commPosts);
 })
 
 /** Handler for creating a community post */
 router.post('/store', (req,res) =>{
-   CommunityPosts.create(req.body, (error, communityPost)=>{
-      console.log(req.body);
-      if(error){
-         console.error(error);
-      }
-   });
-   res.send(req.body);
+   const commPost = CommunityPosts.create(req.body);
+   res.send(commPost);
 })
 /**
  *  
  */
-router.put('update/:id', async(req,res)=>{
-   const communityPost = await CommunityPosts.findByIdAndUpdate(req.params.id, {...req.body});
-   res.send(communityPost);
+router.put('/update/:id', async(req,res)=>{
+   const commPost = await CommunityPosts.findByIdAndUpdate(req.params.id, {...req.body});
+   res.send(commPost);
 })
 
 /**
@@ -61,9 +57,9 @@ router.put('update/:id', async(req,res)=>{
  * based on _id passed from req.params
  * console logs the deleted record and redirects to communityPosts page
  */
-router.delete('delete/:id', async(req, res)=>{
-    const communityPost = await CommunityPosts.findByIdAndDelete(req.params.id);  
-    res.send(communityPost);
+router.delete('/delete/:id', async(req, res)=>{
+    const commPost = await CommunityPosts.findByIdAndDelete(req.params.id);  
+    res.send(commPost);
 })
 
 module.exports = router;
