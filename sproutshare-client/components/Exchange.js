9ab3b0356423ccea972c743f1fd5411ff2608a3d
erikgooding1 <erik.gooding1@gmail.com>
2022-09-25 22:24:47 -0400

    Testing basic modal in Exchange listing

--- sproutshare-client/components/Exchange.js
+++ sproutshare-client/components/Exchange.js
@@ -1,20 +1,42 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import{SafeAreaView, Text, View, Button, FlatList, StyleSheet } from 'react-native';
+import{SafeAreaView, Text, View, Button, FlatList, StyleSheet, Modal, TextInput, Pressable } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'
 
 
 class Exchange extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            data: [],
+            modalIsVisible: false
         }
+        this.setModalIsVisible = this.setModalIsVisible.bind(this);
+        this.setModalIsInvisible = this.setModalIsInvisible.bind(this);
+    }
+
+    setModalIsVisible() {
+        this.setState({modalIsVisible: true});
+    }
+
+    setModalIsInvisible() {
+        this.setState({modalIsVisible: false});
     }
 
     render() {
         return(
             <View styles={styles.container}>
                 <Text>Welcome to the Plant Exchange</Text>
+                <Button title='Search for post' color='#228b22' onPress={this.setModalIsVisible}/>
+                <Modal style={styles.modalcss} visible={this.state.modalIsVisible} animationType='slide'>
+                    <Button title='Exchange Home' color='#228b22' onPress={this.setModalIsInvisible}/>
+                    
+                    <TextInput
+                        style={styles.textInput} 
+                        placeholder='Search by plant, user, or description' 
+                    />
+                </Modal>
+
                 <FlatList 
                     data={this.state.data}
                     renderItem={({ item }) => 
@@ -54,6 +76,36 @@ const styles = StyleSheet.create({
     title: {
       fontSize: 16,
     },
+    textInput: {
+        borderWidth: 1,
+        borderRadius: 6,
+        padding: 16,
+        backgroundColor: '#B2D3C2',
+        borderColor: '#B2D3C2',
+        width: '100%',
+        padding: 8,
+        color: '#120438'
+    },
+
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2
+    },
+    buttonOpen: {
+        backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+        backgroundColor: "#2196F3",
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+
+
+
   });
 
 export default Exchange;
