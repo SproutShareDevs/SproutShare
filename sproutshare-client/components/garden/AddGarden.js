a66cbf3235c2573024bb4a73ea081b955a4b55ec
Alex Teller <atell006@odu.edu>
2022-10-24 05:06:06 -0400

    Added the ability to add soil type with a garden and a "questionnaire"

--- sproutshare-client/components/garden/AddGarden.js
+++ sproutshare-client/components/garden/AddGarden.js
@@ -11,6 +11,7 @@ function AddGarden(props) {
     const [lightLevel, setLightLevel] = useState(10);
     const [userKey, setUserKey] = useState();
     const [soilType, setSoilType] = useState(0);
+    const [soilQuest, toggleSoilQuest] = useState(false);
   
     useEffect(() => {
         const fetchUserKey = async () => {
@@ -27,6 +28,8 @@ function AddGarden(props) {
     async function postGarden () {
         await axios.post(`${props.nodeServer}/gardens/store`, {
             user_key: userKey,
+            soil_key: soilType,
+            light_level: lightLevel,
         }).then((response) => {
             console.log(response);
         }).catch(err => {
@@ -39,6 +42,44 @@ function AddGarden(props) {
         <Button color='#228b22' title='Add Garden' onPress={() => toggleGardenModal(true)}/>
         <Modal visible={gardenModal} animationType="slide">
             <View style={styles.button}>
+
+                {/* Replace this with something better, someday */}
+                <Button title='Soil questionnaire' onPress={() => toggleSoilQuest(true)}/>
+                <Modal visible={soilQuest} animationType="slide">
+                    <Text>Don't know your soil type? Fill out our questionnaire!</Text>
+                    <View style={{ flexDirection:"row", justifyContent: "center" }}>
+                            <Button title='1' onPress={()=> setSoilType(1)}></Button>
+                            <Button title='2' onPress={()=> setSoilType(2)}></Button>
+                            {/* These should be added when those types are added to the DB */}
+                            {/* <Button title='3' onPress={()=> setSoilType(3)}></Button>
+                            <Button title='4' onPress={()=> setSoilType(4)}></Button> */}
+                    </View>
+                    <Button color='red' title='Close' onPress={() => toggleSoilQuest(false)}/>
+                </Modal>
+
+                <Text>Select soil type:</Text>
+                <View style={{ flexDirection:"row", justifyContent: "center" }}>
+                        <Button title='1' onPress={()=> setSoilType(1)}></Button>
+                        <Button title='2' onPress={()=> setSoilType(2)}></Button>
+                        {/* These should be added when those types are added to the DB */}
+                        {/* <Button title='3' onPress={()=> setSoilType(3)}></Button>
+                        <Button title='4' onPress={()=> setSoilType(4)}></Button> */}
+                </View>
+
+                <Text>Select light level:</Text>
+                <View style={{ flexDirection:"row", justifyContent: "center" }}>
+                        <Button title='0' onPress={()=> setLightLevel(0)}></Button>
+                        <Button title='1' onPress={()=> setLightLevel(1)}></Button>
+                        <Button title='2' onPress={()=> setLightLevel(2)}></Button>
+                        <Button title='3' onPress={()=> setLightLevel(3)}></Button>
+                        <Button title='4' onPress={()=> setLightLevel(4)}></Button>
+                        <Button title='5' onPress={()=> setLightLevel(5)}></Button>
+                        <Button title='6' onPress={()=> setLightLevel(6)}></Button>
+                        <Button title='7' onPress={()=> setLightLevel(7)}></Button>
+                        <Button title='8' onPress={()=> setLightLevel(8)}></Button>
+                        <Button title='9' onPress={()=> setLightLevel(9)}></Button>
+                </View>
+
                 <Button color='green' title='Create New Garden' onPress={()=> postGarden()}/>
             </View>
             <View style={styles.button}>
