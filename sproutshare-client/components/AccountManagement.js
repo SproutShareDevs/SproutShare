cbd9f04dbb97014889b991b4b5c22bd118792532
mbred002 <91864052+mbred002@users.noreply.github.com>
2022-11-10 20:46:16 -0500

    Removed old commented out code

--- sproutshare-client/components/AccountManagement.js
+++ sproutshare-client/components/AccountManagement.js
@@ -68,37 +68,6 @@ function AccountManagement(props) {
          
     }
 
-    async function registerForPushNotificationsAsync() {
-        let token;
-      
-        if (Platform.OS === 'android') {
-          await Notifications.setNotificationChannelAsync('default', {
-            name: 'default',
-            importance: Notifications.AndroidImportance.MAX,
-            vibrationPattern: [0, 250, 250, 250],
-            lightColor: '#FF231F7C',
-          });
-        }
-      
-        if (Device.isDevice) {
-          const { status: existingStatus } = await Notifications.getPermissionsAsync();
-          let finalStatus = existingStatus;
-          if (existingStatus !== 'granted') {
-            const { status } = await Notifications.requestPermissionsAsync();
-            finalStatus = status;
-          }
-          if (finalStatus !== 'granted') {
-            alert('Failed to get push token for push notification!');
-            return;
-          }
-          token = (await Notifications.getExpoPushTokenAsync()).data;
-          console.log(token);
-        } else {
-          alert('Must use physical device for Push Notifications');
-        }
-      
-        return token;
-      }
       const triggerLocalNotificationHandler = () => {
         Notifications.scheduleNotificationAsync({
           content: {
@@ -108,14 +77,6 @@ function AccountManagement(props) {
           trigger: { seconds: 5 },
         })
       }
-      const [expoPushToken, setExpoPushToken] = useState('');
-      const [notification, setNotification] = useState(false);
-      const notificationListener = useRef();
-      const responseListener = useRef();
-
-      useEffect(() => {
-        registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
-      }, []);
 
         return (
             <View>
