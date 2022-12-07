8f0ae07a66f27f36e777cca5b4d6461913b6e491
shaylastorm <ssher019@odu.edu>
2022-12-07 03:10:43 -0500

    new layout for adding a new garden

--- sproutshare-client/components/garden/AddGarden.js
+++ sproutshare-client/components/garden/AddGarden.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { View, Text, Image, Modal, Button, TouchableOpacity } from 'react-native'
+import { View, Text, Image, Modal, Button, TouchableOpacity, ImageBackground } from 'react-native'
 import { FlatList, TextInput } from 'react-native-gesture-handler';
 import styles from '../../styles/styles';
 import axios from 'axios';
@@ -49,28 +49,36 @@ function AddGarden(props) {
             </TouchableOpacity>
 
             <Modal visible={gardenModal} animationType="slide">
+            <ImageBackground source={require("./../../assets/MainBackground.png")} style={styles.backgroundImage}>
                 <View style={styles.button}>
-
+                
                     <SoilQuestionnaire {...props} setSoilType={setSoilType} soilType={soilType} />
 
                     <ChooseSoilType {...props} setSoilType={setSoilType} soilType={soilType} />
-                    <Text>Current soil type: {soilType}</Text>
+                    <Text style={{fontSize:15,marginLeft:125, fontWeight:'bold'}}>Current soil type: {soilType}</Text>
 
                     <ChooseLightLevel {...props} setLightLevel={setLightLevel} />
-                    <Text>Current light level: {lightLevel}</Text>
+                    <Text style={{fontSize:15, marginTop:10,marginLeft:125, fontWeight:'bold'}}>Current light level: {lightLevel}</Text>
 
                     <>
-                        <Text> Garden name:</Text>
+                        <Text style={{fontSize:18, fontWeight:'bold', marginTop:10}}> Garden name:</Text>
                         <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
                             <TextInput style={styles.textInput} onChangeText={setGardenName} placeholder={'Choose garden name'} />
                         </View>
                     </>
+                <TouchableOpacity
+                    onPress={() => postGarden()} 
+                    style={[styles.newGarden, {backgroundColor: '#5ab07d'}]}>
+                    <Text style={[styles.buttonText]}>Create New Garden</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => toggleGardenModal(false)} 
+                    style={[styles.newGarden, {backgroundColor: '#D4CD68'}]}>
+                    <Text style={styles.buttonText}>Cancel</Text>
+                </TouchableOpacity>
 
-                    <Button color='green' title='Create New Garden' onPress={() => postGarden()} />
                 </View>
-                <View style={styles.button}>
-                    <Button color='red' title='Close' onPress={() => toggleGardenModal(false)} />
-                </View>
+            </ImageBackground>
             </Modal>
         </>
     );
@@ -79,18 +87,18 @@ function AddGarden(props) {
 function ChooseLightLevel(props) {
     return (
         <>
-            <Text>Select light level:</Text>
-            <View style={{ flexDirection: "row", justifyContent: "center" }}>
-                <Button title='0' onPress={() => props.setLightLevel(0)}></Button>
-                <Button title='1' onPress={() => props.setLightLevel(1)}></Button>
-                <Button title='2' onPress={() => props.setLightLevel(2)}></Button>
-                <Button title='3' onPress={() => props.setLightLevel(3)}></Button>
-                <Button title='4' onPress={() => props.setLightLevel(4)}></Button>
-                <Button title='5' onPress={() => props.setLightLevel(5)}></Button>
-                <Button title='6' onPress={() => props.setLightLevel(6)}></Button>
-                <Button title='7' onPress={() => props.setLightLevel(7)}></Button>
-                <Button title='8' onPress={() => props.setLightLevel(8)}></Button>
-                <Button title='9' onPress={() => props.setLightLevel(9)}></Button>
+            <Text style={{marginTop:10, marginBottom:10,fontSize:18, fontWeight:'bold'}}>Select light level:</Text>
+            <View style={{ flexDirection: "row", justifyContent: "center", marginRight:5 }}>
+                <Button color='#FFF26C' title='0' onPress={() => props.setLightLevel(0)}></Button>
+                <Button color='#FFF05A' title='1' onPress={() => props.setLightLevel(1)}></Button>
+                <Button color='#FFEF4B' title='2' onPress={() => props.setLightLevel(2)}></Button>
+                <Button color='#FFEE3A' title='3' onPress={() => props.setLightLevel(3)}></Button>
+                <Button color='#FFED2B' title='4' onPress={() => props.setLightLevel(4)}></Button>
+                <Button color='#FFE11F' title='5' onPress={() => props.setLightLevel(5)}></Button>
+                <Button color='#F9D908' title='6' onPress={() => props.setLightLevel(6)}></Button>
+                <Button color='#F9CD08' title='7' onPress={() => props.setLightLevel(7)}></Button>
+                <Button color='#FFC80E' title='8' onPress={() => props.setLightLevel(8)}></Button>
+                <Button color='#FDC403' title='9' onPress={() => props.setLightLevel(9)}></Button>
             </View>
         </>
     )
@@ -99,13 +107,33 @@ function ChooseLightLevel(props) {
 function ChooseSoilType(props) {
     return (
         <>
-            <Text>Select soil type:</Text>
+            <Text style={{fontSize:18, fontWeight: 'bold'}}>Select soil type:</Text>
             <View style={{ flexDirection: "row", justifyContent: "center" }}>
-                <Button title='Silty' onPress={() => props.setSoilType(1)}></Button>
-                <Button title='Clay' onPress={() => props.setSoilType(2)}></Button>
-                {/* These should be added when those types are added to the DB */}
-                <Button title='Sandy' onPress={()=> props.setSoilType(3)}></Button>
-                <Button title='Loamy' onPress={()=> props.setSoilType(4)}></Button> 
+                <TouchableOpacity style={styles.soilButton}
+                    onPress={() => props.setSoilType(1)}>
+                    <ImageBackground source={require("./../../assets/silty.png")} style={styles.buttonImage}>
+                    </ImageBackground>
+                    <Text style={styles.gardenButtonText}>Silty</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.soilButton}
+                    onPress={() => props.setSoilType(2)}>
+                    <ImageBackground source={require("./../../assets/clay.png")} style={styles.buttonImage}>
+                    </ImageBackground>
+                    <Text style={styles.gardenButtonText}>Clay</Text>
+                  </TouchableOpacity>
+                   {/* These should be added when those types are added to the DB */}
+                  <TouchableOpacity style={styles.soilButton}
+                    onPress={()=> props.setSoilType(3)}>
+                    <ImageBackground source={require("./../../assets/sand.png")} style={styles.buttonImage}>
+                    </ImageBackground>
+                    <Text style={styles.gardenButtonText}>Sandy</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity style={styles.soilButton}
+                    onPress={()=> props.setSoilType(4)}>
+                    <ImageBackground source={require("./../../assets/loam.png")} style={styles.buttonImage}>
+                    </ImageBackground>
+                    <Text style={styles.gardenButtonText}>Loamy</Text>
+                  </TouchableOpacity>
             </View>
         </>
     )
@@ -115,7 +143,13 @@ function SoilQuestionnaire(props) {
     const [soilQuest, toggleSoilQuest] = useState(false);
     return (
         <>
-            <Button title='Soil questionnaire' onPress={() => toggleSoilQuest(true)} />
+            <Text style={styles.soilQ}> Dont know your soil type? Take this Quiz.</Text>
+            <TouchableOpacity
+                    onPress={() => toggleSoilQuest(true)}
+                    style={[styles.quizButton, {backgroundColor: '#A5B8A1'}]}>
+                    <Text style={styles.buttonText}>soil questionare</Text>
+                  </TouchableOpacity>
+                  
             <Modal visible={soilQuest} animationType="slide">
                 <Text>Don't know your soil type? Fill out our questionnaire!</Text>
                 <Text>Pick some wet soil and squish it. Does it feel...</Text>
